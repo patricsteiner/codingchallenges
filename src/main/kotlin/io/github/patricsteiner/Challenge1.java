f5518a03fb0eb9552cd83b0bfe8cb9884c7e1d7b
Patric Steiner <patric.steiner1@gmail.com>
2020-11-19 00:06:35 +0100

    add first challenge

---
+++ src/main/kotlin/io/github/patricsteiner/Challenge1.java
@@ -0,0 +1,14 @@
+package io.github.patricsteiner;
+
+public class Challenge1 {
+
+    /**
+     * @param banks   List of bank coordinates in the form "x1,y1;x2,y2;x3,y3"
+     * @param partner Partner coordinate in the form "x,y"
+     * @return The zero-based index of the first bank closest to the partner (euclidean distance)
+     */
+    int findClosestBank(String banks, String partner) {
+        return 0; // TODO your code here :)
+    }
+
+}
